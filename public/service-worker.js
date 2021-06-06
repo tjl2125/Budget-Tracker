153d52b12a23d7b0ed14b83e8f231b1b88f01136
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-05 23:11:38 -0400

    adding db.js and filling out the service worker

---
+++ public/service-worker.js
@@ -0,0 +1,71 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/index.js',
+    '/db.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  // install\
+  self.addEventListener("install", function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/transaction ")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
